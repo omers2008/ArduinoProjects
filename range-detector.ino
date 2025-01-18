9dbe6b838c2880ddecdbebc0d52116f150e6c266
omers2008 <omer.schwartz.2008@gmail.com>
2025-01-18 12:08:55 +0200

    reformat

--- range-detector.ino
+++ range-detector.ino
@@ -8,21 +8,23 @@ const int ECHO_PIN = 8;
 long duration;
 int distance;
 const int BUZZ_PIN = 7;
-const char* LOW_STATUS = "LOW";
-const char* MID_STATUS = "MID";
-const char* HIGH_STATUS = "HIGH";
-const char* VALUE_LABEL = "Distance:";
+const char *LOW_STATUS = "LOW";
+const char *MID_STATUS = "MID";
+const char *HIGH_STATUS = "HIGH";
+const char *VALUE_LABEL = "Distance:";
 const int TIMEOUT = 25000; // 25 milliseconds, a bit more than the round trip at max range
 
-void setup() {
+void setup()
+{
   pinMode(TRIG_PIN, OUTPUT); // Sets the trigPin as an Output
-  pinMode(ECHO_PIN, INPUT); // Sets the echoPin as an Input
+  pinMode(ECHO_PIN, INPUT);  // Sets the echoPin as an Input
   pinMode(BUZZ_PIN, OUTPUT); // Sets the trigPin as an Output
   Serial.begin(9600);
   lcd.begin(16, 2);
 }
 
-void loop() {
+void loop()
+{
   delay(150);
   // Clears the trigPin
   digitalWrite(TRIG_PIN, LOW);
@@ -35,38 +37,51 @@ void loop() {
   // Reads the echoPin, returns the sound wave travel time in microseconds
   duration = pulseIn(ECHO_PIN, HIGH, TIMEOUT);
 
-  if (duration == 0) {  // Check if timeout occurred
-    distance = -1;  // Use -1 or another sentinel value to indicate no valid echo was received
-  } else {
+  if (duration == 0)
+  {                // Check if timeout occurred
+    distance = -1; // Use -1 or another sentinel value to indicate no valid echo was received
+  }
+  else
+  {
     // Calculating the distance
     distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and return)
   }
 
   lcd.setCursor(0, 1);
-  if (distance == -1) {
+  if (distance == -1)
+  {
     lcd.print("Out of range ");
-  } else {
+  }
+  else
+  {
     lcd.print(VALUE_LABEL);
     lcd.print(distance);
     lcd.print(" cm  ");
   }
 
-  if (distance < 50 && distance != -1) {
+  if (distance < 50 && distance != -1)
+  {
     lcd.setCursor(0, 0);
     lcd.print(LOW_STATUS);
     lcd.print("   ");
     digitalWrite(BUZZ_PIN, HIGH);
-  } else if (distance >= 50 && distance < 100) {
+  }
+  else if (distance >= 50 && distance < 100)
+  {
     lcd.setCursor(0, 0);
     lcd.print(MID_STATUS);
     lcd.print("   ");
     digitalWrite(BUZZ_PIN, LOW);
-  } else if (distance >= 100) {
+  }
+  else if (distance >= 100)
+  {
     lcd.setCursor(0, 0);
     lcd.print(HIGH_STATUS);
     lcd.print("   ");
     digitalWrite(BUZZ_PIN, LOW);
-  } else {
+  }
+  else
+  {
     lcd.setCursor(0, 0);
     lcd.print("ERROR      ");
     digitalWrite(BUZZ_PIN, LOW);
